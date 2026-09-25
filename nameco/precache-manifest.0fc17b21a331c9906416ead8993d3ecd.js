f2686f2675f29e5fc2aafc031d8661c1b8d21efc
wiiwu959 <wiiwu959@gmail.com>
2026-09-25 13:08:37 +0100

    add nameco/ - mushroom nest party calculator (2026 data refresh)

---
+++ nameco/precache-manifest.0fc17b21a331c9906416ead8993d3ecd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "v104-update2026",
+    "url": "static/js/main.02f9f507.chunk.js"
+  },
+  {
+    "revision": "99cad2d4cfaa005f630e",
+    "url": "static/js/1.99cad2d4.chunk.js"
+  },
+  {
+    "revision": "v104-update2026",
+    "url": "static/css/main.d5f21c49.chunk.css"
+  },
+  {
+    "revision": "index-v104",
+    "url": "index.html"
+  }
+];

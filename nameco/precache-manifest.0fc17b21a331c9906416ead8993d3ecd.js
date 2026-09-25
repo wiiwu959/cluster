5a20853fa7834dad47121a868bb88a6a1c27f281
wiiwu959 <wiiwu959@gmail.com>
2026-09-25 22:43:12 +0100

    feat(nameko): use initial stats and numeric input

--- nameco/precache-manifest.0fc17b21a331c9906416ead8993d3ecd.js
+++ nameco/precache-manifest.0fc17b21a331c9906416ead8993d3ecd.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "229c360febb4351a89df",
-    "url": "static/js/runtime~main.229c360f.js"
+    "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "v104-update2026",
-    "url": "static/js/main.02f9f507.chunk.js"
+    "revision": "v105-initial-input-2026",
+    "url": "/static/js/main.02f9f507.chunk.js"
   },
   {
     "revision": "99cad2d4cfaa005f630e",
-    "url": "static/js/1.99cad2d4.chunk.js"
+    "url": "/static/js/1.99cad2d4.chunk.js"
   },
   {
     "revision": "v104-update2026",
-    "url": "static/css/main.d5f21c49.chunk.css"
+    "url": "/static/css/main.d5f21c49.chunk.css"
   },
   {
     "revision": "index-v104",
-    "url": "index.html"
+    "url": "/index.html"
   }
 ];
